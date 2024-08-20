585efd50bdbd229ad333eaeddce0afba824b8607
Sachin Ramsamy <sachin_drz125@hotmail.com>
2024-08-20 13:50:49 +1000

    Display fidoutilsConfig object in side panel

--- middle.js
+++ middle.js
@@ -60,3 +60,78 @@ async function retrieveFidoUtilsConfigFromBackgroundScript() {
 //   }
 //   return await response;
 // }
+
+// Interaction with side panel
+
+// function listenForRegisterButtonClick() {
+
+// }
+let button = document.getElementById("triggerBtn");
+console.log("find me button listener")
+if (button) {
+  button.addEventListener("click", async function () {
+    console.log("Clicked button");
+    let data = await retrieveFidoUtilsConfigFromBackgroundScript();
+    await displayFidoUtilsConfigObject(data);
+  });
+}
+
+async function displayFidoUtilsConfigObject(o) {
+  // console.log("this is the displayFidoUtilsConfigObject function", o);
+
+  // Display the encryption passphrase
+  let displayencryptionPassphrase = document.getElementById("encryptionPassphrase");
+  if (displayencryptionPassphrase) {
+    displayencryptionPassphrase.innerHTML = `<h4> Encryption Passphrase: </h4> ${o["encryptionPassphrase"]} `;
+  }
+  // Display the fido-u2f data
+  let displayfidou2f = document.getElementById("cert");
+  if (displayfidou2f) {
+    displayfidou2f.innerHTML = `<h4> cert: </h4> ${o["fido-u2f"].cert}`;
+  }
+  let displayPrivateKeyHex = document.getElementById("privateKeyHex");
+  if (displayPrivateKeyHex) {
+    displayPrivateKeyHex.innerHTML = `<h4> Private Key: </h4> ${o["fido-u2f"].privateKeyHex}`;
+  }
+
+  let displayPublicKeyHex = document.getElementById("publicKeyHex");
+  if (displayPublicKeyHex) {
+    displayPublicKeyHex.innerHTML = `<h4> Public Key: </h4> ${o["fido-u2f"].publicKeyHex}`;
+  }
+
+  // Display the packed data
+  let displayPackedAaguid = document.getElementById("packed-aaguid");
+  if (displayPackedAaguid) {
+    displayPackedAaguid.innerHTML = `<h4> Packed aaguid: </h4> ${o["packed"].aaguid}`;
+  }
+
+  let displayPackedCert = document.getElementById("packed-cert");
+  if (displayPackedCert) {
+    displayPackedCert.innerHTML = `<h4> Packed Cert: </h4>${o["packed"].cert}`;
+  }
+
+  let displayPackedPrivateKeyHex = document.getElementById("packed-privateKeyHex");
+  if (displayPackedPrivateKeyHex) {
+    displayPackedPrivateKeyHex.innerHTML = `<h4> Packed Private Key: </h4> ${o["packed"].privateKeyHex}`;
+  }
+
+  let displayPackedPublicKeyHex = document.getElementById("packed-publicKeyHex");
+  if (displayPackedPublicKeyHex) {
+    displayPackedPublicKeyHex.innerHTML = `<h4> Packed Public Key: </h4> ${o["packed"].publicKeyHex}`;
+  }
+  // Dispaly the packed-self aaguid
+  let displayPackedSelfAaguid = document.getElementById("packed-self-aaguid");
+  if (displayPackedSelfAaguid) {
+    displayPackedSelfAaguid.innerHTML = `<h4> Packed-self aaguid: </h4> ${o["packed-self"].aaguid}`;
+  }
+
+
+}
+
+
+
+
+
+
+
+
