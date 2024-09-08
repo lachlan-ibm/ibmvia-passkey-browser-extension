34133da8fa05082380d4c2de23e1f4c0b650911f
Sachin Ramsamy <sachin_drz125@hotmail.com>
2024-09-08 19:03:30 +1000

    Add BrowserApi abstraction

--- copy_generate_attestation_certs.js
+++ copy_generate_attestation_certs.js
@@ -396,4 +396,27 @@ function setFido2ClientConfigJSON(updatedObj) {
     fido2ClientConfigJSON = updatedObj;
 };
 
-module.exports = { getFido2ClientConfigJSON: getFido2ClientConfigJSON, setFido2ClientConfigJSON, setFido2ClientConfigJSON };
+// browserApi.js
+class BrowserApi {
+    static isChromeApi = navigator.userAgent.includes("Chrome");
+    static isFirefoxApi = navigator.userAgent.includes("Firefox");
+
+    static get runtime() {
+        return this.isChromeApi ? chrome.runtime : browser.runtime;
+    }
+
+    static get sidePanel() {
+        if (this.isChromeApi) {
+            return chrome.sidePanel;
+        } else if (this.isFirefoxApi) {
+            return browser.sidebarAction;
+        }
+        return null;
+    }
+}
+
+module.exports = {
+    getFido2ClientConfigJSON: getFido2ClientConfigJSON,
+    setFido2ClientConfigJSON, setFido2ClientConfigJSON,
+    BrowserApi: BrowserApi
+};
