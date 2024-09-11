8f11ec71c6e34a270a890b1010f699f83281c30e
Sachin Ramsamy <sachin_drz125@hotmail.com>
2024-09-11 14:11:33 +1000

    Create first test script using selenium

---
+++ test/firstScript.js
@@ -0,0 +1,31 @@
+const { Builder, Browser, By } = require("selenium-webdriver");
+const assert = require("assert");
+
+(async function firstTest() {
+    let driver;
+    try {
+        driver = await new Builder().forBrowser(Browser.CHROME).build();
+        await driver.get("https://webauthn.io/");
+        let title = await driver.getTitle();
+        assert.equal("WebAuthn.io", title)
+        await driver.manage().setTimeouts({ implicit: 500 });
+        let inputBox = await driver.findElement(By.id("input-email"));
+        let registerBtn = await driver.findElement(By.id("register-button"));
+
+        // Take action on element
+        await inputBox.sendKeys("SeleniumTest");
+        await registerBtn.click();
+
+        // Request element information
+        // let value = await message.getText();
+    } catch (e) {
+        console.log(e);
+    } finally {
+        // End the session
+        // await driver.quit();
+    }
+}())
+
+
+
+
